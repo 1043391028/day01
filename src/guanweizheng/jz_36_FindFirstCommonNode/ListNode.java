b71fbad6f29aa9cc098d2b9632f8a5cf249e36be
gwzstar <guanweizheng66@163.com>
2020-09-19 23:22:02 +0800

    剑指offer36题

---
+++ src/guanweizheng/jz_36_FindFirstCommonNode/ListNode.java
@@ -0,0 +1,10 @@
+package guanweizheng.jz_36_FindFirstCommonNode;
+
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
---
+++ src/guanweizheng/jz_36_FindFirstCommonNode/Test.java
@@ -0,0 +1,30 @@
+package guanweizheng.jz_36_FindFirstCommonNode;
+
+public class Test {
+    public static void main(String[] args) {
+        ListNode list1 = new ListNode(0);
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        list1.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+
+        ListNode list2 = new ListNode(7);
+        ListNode node6 = new ListNode(8);
+        ListNode node7 = new ListNode(4);
+        ListNode node8 = new ListNode(5);
+        list2.next = node6;
+        node6.next = node7;
+        node7.next = node8;
+
+        Solution solution = new Solution();
+        ListNode target = solution.FindFirstCommonNode(list1,list2);
+//        int num = target.val;
+        System.out.println("第一个公共节点的值为："+target.val);
+    }
+}
