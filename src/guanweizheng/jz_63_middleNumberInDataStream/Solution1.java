93214f8e95c9c8608943edd2a2a02b67a063d40e
gwzstar <guanweizheng66@163.com>
2020-10-04 23:37:28 +0800

    剑指offer更新20.10.04

--- src/guanweizheng/jz_63_middleNumberInDataStream/Solution1.java
+++ src/guanweizheng/jz_63_middleNumberInDataStream/Solution1.java
@@ -4,18 +4,30 @@ package guanweizheng.jz_63_middleNumberInDataStream;
 //             如果从数据流中读出偶数个数值，那么中位数就是所有数值排序之后中间两个数的平均值。我们使用Insert()方法读取数据流，
 //             使用GetMedian()方法获取当前读取数据的中位数。
 
+import javax.swing.plaf.basic.BasicSliderUI;
+import java.text.CollationElementIterator;
+import java.util.ArrayList;
+import java.util.Collections;
+
 //        方法一：使用无序数组存储，然后需要对数组进行排序，然后就可以顺利找到中位数。
 //                使用库函数对无序数组排序，然后根据下标找到中位数；
 public class Solution1 {
 
+    ArrayList<Integer> list = new ArrayList<>();
     public void Insert(Integer num) {
-
+        list.add(num);
     }
 
     public Double GetMedian() {
-
-        return null;
-
+        Double result;
+        int index = list.size()/2;
+        Collections.sort(list);
+//        个数为奇数
+        if(index%2 != 0){
+            return (double)list.get(index);
+        }
+//        个数为偶数
+        return (double)(list.get(index)+list.get(index+1));
     }
 
 }
--- src/guanweizheng/jz_63_middleNumberInDataStream/Solution2.java
+++ src/guanweizheng/jz_63_middleNumberInDataStream/Solution2.java
@@ -3,14 +3,35 @@ package guanweizheng.jz_63_middleNumberInDataStream;
 //         方法二：在插入数组时，就进行排序，形成有序的数组，排完序后就可以直接找到中位数
 //                 插入的时候排序，所以很容易想到利用堆排序，每次加入一个元素就调整堆。因为第4种使用到了堆，
 //                 所以这里选择（在网上扒了）另一种插入排序的方法，二分法。
+import java.util.LinkedList;
 
 public class Solution2 {
+    LinkedList<Integer> list = new LinkedList<>();
     public void Insert(Integer num) {
+//        如果链表为空，直接插入；
+        if(list.size()==0){
+            list.add(num);
+            return;
+        }
+        int start = 0,end  = list.size()-1,mid = 0;
+//        二分法查找插入结点下标
+        while(start <= end){
+            mid = (start + end)/2;
+            if(num < list.get(mid)){
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+        }
 
+        list.add(mid,num);
+        return;
     }
 
     public Double GetMedian() {
-        return null;
-
+        int index = list.size();
+        if(index%2 != 0)
+            return (double)list.get(index/2);
+        return (double)(list.get(index/2)+list.get(index/2+1));
     }
 }
