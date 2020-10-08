e3c92cda797be7819479531613d89392598be8b1
gwzstar <guanweizheng66@163.com>
2020-10-09 00:27:39 +0800

    剑指offer更新20.10.09

--- src/guanweizheng/jz_33_getUgulyNumber/Solution.java
+++ src/guanweizheng/jz_33_getUgulyNumber/Solution.java
@@ -2,9 +2,10 @@ package guanweizheng.jz_33_getUgulyNumber;
 
 //           题目描述:  把只包含质因子2、3和5的数称作丑数（Ugly Number）。例如6、8都是丑数，但14不是，因为它包含质因子7。
 //                      习惯上我们把1当做是第一个丑数。求按从小到大的顺序的第N个丑数。
-//               思路:   质因子只能为2,3,5,第一个丑数1,1*(2,3,5)得到丑数,然后2*(2,3,5)=(4,6,10)也为丑数,3*(2,3,5,)=(6,9,15)
-//                        为下一轮丑数,但是,这三轮丑数并不是按大小拍好的,所以这样直接得到,需要判断每一轮最小的数,
-//                        然后将每一轮最小的下标+1,进入下一轮判断;  具体如下:
+//               思路:   质因子只能为2,3,5,那么,所有的丑数*(2,3,5)得到的数也为丑数,所以可以维护三个数组p2[],p3[],p5[]
+//                       p2[] = {2*1,2*2,2*3,2*4,2*5,2*6,2*8.....},p3[] = {3*1,3*2,3*3,3*4,3*5,3*6,3*8...}
+//                       p5[] = {5*1,5*2,5*3,5*4,5*5,5*6,5*7.....}
+//                       但是,这三个数组的结果顺序需要重新排列,因为小的需要先进数组
 
 public class Solution {
            public int getUglyNumber(int index){
@@ -14,10 +15,10 @@ public class Solution {
                int p2 = 0,p3 = 0, p5 = 0;
 //               遍历到下标,得到第index个丑数;
                for(int i= 1 ; i < index; i++){
-                   result[i] = Math.min(2*result[p2], Math.min(3*result[p3], 5*result[p5]));
-                   if(result[i] == 2*result[p2]) p2++;
-                   if(result[i] == 3*result[p3]) p3++;
-                   if(result[i] == 5*result[p5]) p5++;
+                   result[i] = Math.min(2*result[p2], Math.min(3*result[p3], 5*result[p5]));//比较三个数组大小
+                   if(result[i] == 2*result[p2]) p2++;  // 相当于p2[] 数组在不停的更新,p2++,result[p2]下一个最小的丑数
+                   if(result[i] == 3*result[p3]) p3++;  // 相当于p3[] 数组在不停的更新
+                   if(result[i] == 5*result[p5]) p5++;  // 相当于p5[] 数组在不停的更新
                }
                return result[index-1];
 
