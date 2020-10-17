e1d3ed060e7f994389626da79cd225a9ba588de4
gwzstar <guanweizheng66@163.com>
2020-10-17 22:53:57 +0800

    剑指offer更新20.10.17

--- src/guanweizheng/jz_20_includ_minFunctionStack/Test.java
+++ src/guanweizheng/jz_20_includ_minFunctionStack/Test.java
@@ -2,6 +2,7 @@ package guanweizheng.jz_20_includ_minFunctionStack;
 
 public class Test {
     public static void main(String[] args) {
+
         Solution sl =new Solution();
         sl.push(4);
         sl.push(2);
@@ -12,6 +13,7 @@ public class Test {
         System.out.println("栈顶元素为："+top+"\n栈里面最小元素为："+min);
         sl.pop();
         min = sl.min();top = sl.top();
+
         System.out.println("栈顶元素为："+top+"\n栈里面最小元素为："+min);
         sl.pop();
         min = sl.min();top = sl.top();
@@ -19,9 +21,6 @@ public class Test {
         sl.pop();
         min = sl.min();top = sl.top();
         System.out.println("栈顶元素为："+top+"\n栈里面最小元素为："+min);
-
-
-
     }
 
 
